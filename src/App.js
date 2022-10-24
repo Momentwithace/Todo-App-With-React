1aff7afb0e86cdb2359e18d58343912e884a6b93
momemtwithace <augustineezekiel763@gmail.com>
2022-10-24 10:33:52 +0000

    TODO

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import AddTaskForm from './components/AddTaskForm.jsx';
+import UpdateForm from './components/UpdateForm.jsx';
+import ToDo from './components/ToDo.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
+
+  // Tasks (ToDo List) State
+  const [toDo, setToDo] = useState([]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  // Add task 
+
+  const addTask = () => {
+    if(newTask) {
+      let num = toDo.length + 1; 
+      let newEntry = { id: num, title: newTask, status: false }
+      setToDo([...toDo, newEntry])
+      setNewTask('');
+    }
+  }
+
+
+  // Mark task as done or completed
+  
+  const markDone = (id) => {
+    let newTask = toDo.map( task => {
+      if( task.id === id ) {
+        return ({ ...task, status: !task.status })
+      }
+      return task;
+    })
+    setToDo(newTask);
+  }
+
+  // Cancel update
+  
+  const cancelUpdate = () => {
+    setUpdateData('');
+  }
+
+  // Change task for update
+
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  }
+
+  //  Update task
+  
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter( task => task.id !== updateData.id );
+    let updatedObject = [...filterRecords, updateData]
+    setToDo(updatedObject);
+    setUpdateData('');
+  }
+
+
+  // Delete task 
+  
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter( task => task.id !== id)
+    setToDo(newTasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+
+    <br /><br />
+    <h1>Hey! What's your plan for today?</h1>
+    <br /><br />
+
+    {updateData && updateData ? (
+      <UpdateForm 
+        updateData={updateData}
+        changeTask={changeTask}
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}
+      />
+    ) : (
+      <AddTaskForm 
+        newTask={newTask}
+        setNewTask={setNewTask}
+        addTask={addTask}
+      />
+    )}
+
+    {/* Display ToDos */}
+
+    {toDo && toDo.length ? '' : 'No Tasks...'}
+
+    <ToDo
+      toDo={toDo}
+      markDone={markDone}
+      setUpdateData={setUpdateData}
+      deleteTask={deleteTask}
+    />  
+    
+
+    <div className='qoute'>
+    <p>
+        Among the best moment of life, are those moment when you make out plans,
+        and then watch them play out..
+      </p>
+      <p>
+        Write down a plan today....
+      </p>
+      <p>@momentithace</p>
+    </div >
+      
     </div>
   );
 }
